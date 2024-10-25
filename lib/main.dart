dbe5c810d5756b9ee0ad24989193b5b917011d2c
Phuong1809n <21120534@student.hcmus.edu.vn>
2024-10-25 11:09:48 +0700

    update promt lib ui

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 // main.dart
 import 'package:flutter/material.dart';
-import 'views/chat_screen.dart';
+import 'views/prompt_library_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: ChatScreen(),
+      home: PromptLibraryScreen(),
     );
   }
 }
