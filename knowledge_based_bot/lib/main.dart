44ee80e68359258e4028ec22be11b591dd0e23c3
Kiet Chau <chaukietck25@gmail.com>
2024-11-23 09:56:42 +0700

    new thread chat

--- knowledge_based_bot/lib/main.dart
+++ knowledge_based_bot/lib/main.dart
@@ -27,8 +27,8 @@ class MyApp extends StatelessWidget {
         // colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      // home: const OnboardingScreen(),
-      home: const HomePage(),
+      home: const OnboardingScreen(),
+      // home: const HomePage(),
 
     );
   }
