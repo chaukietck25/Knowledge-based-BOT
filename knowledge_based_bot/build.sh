ee6f2bd2cfed9eacd943daa4e0c1809a98a9df5b
Kiet Chau <chaukietck25@gmail.com>
2024-12-19 07:24:04 +0700

    Update build script and configuration

--- knowledge_based_bot/build.sh
+++ knowledge_based_bot/build.sh
@@ -9,7 +9,13 @@ git clone https://github.com/flutter/flutter.git -b stable --depth 1
 # Add Flutter to the PATH
 export PATH="$PATH:`pwd`/flutter/bin"
 
-# Run Flutter commands
-flutter doctor
-flutter pub get
-flutter build web
+# Create a non-root user and switch to that user
+useradd -m flutteruser
+chown -R flutteruser:flutteruser .
+su - flutteruser -c "
+  cd $(pwd)
+  export PATH=\"$PATH\"
+  flutter doctor
+  flutter pub get
+  flutter build web
+"
